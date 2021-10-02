c9281805c4534a9ab5e29d5bc4827124652d1745
mikeOde <mike.r.ayoub@gmail.com>
2021-10-02 13:53:37 +0300

    Login, signup, logout, user_profile

--- sugar-factory-server/app/Models/User.php
+++ sugar-factory-server/app/Models/User.php
@@ -19,7 +19,8 @@ class User extends Authenticatable implements JWTSubject
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
